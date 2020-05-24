2ccf70cb14b6a3c33d0803fffa07be64bbb1a833
Biditmangal <biditmangal9506@gmail.com>
2020-05-24 20:36:06 +0530

    Refactored Projects Screen

--- PortfolioApp/component/Home.js
+++ PortfolioApp/component/Home.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {View,Text,Button,TouchableOpacity} from "react-native";
+import {
+    View,
+    Text,
+} from "react-native";
 import {styles} from "../styles/styles"
 
 export default class Home extends Component {
--- PortfolioApp/component/Project.js
+++ PortfolioApp/component/Project.js
@@ -7,158 +7,173 @@ import {
 } from "react-native";
 import {styles} from "../styles/styles";
 import Panel from "react-native-panel";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 
 
 export default class Project extends Component {
+    state = {
+        projects: []
+    }
+
+
+    dataToRender = () => {
+        let Projects = [...this.state.projects]
+
+        let data = [
+            {
+                id: 1,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 2,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 3,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 4,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 5,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 6,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
+            },
+            {
+                id: 7,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+            },
+            {
+                id: 8,
+                desc: 'Lorem ipsum dolor sit amet,\n' +
+                    'consectetur adipiscistyng elit,\n' +
+                    'sed do eiusmod tempor incididunt ut\n' +
+                    'labore et dolore magna aliqua',
+                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative',
+                orgzn: 'UPES ACM student chapters',
+                duration: 2,
+            },
+        ]
+        Projects = [...data]
+        return Projects;
+    }
+
+    renderListItem = (item) => {
+        return(
+            <Panel
+                header={`Project ${item.id}`}
+                style={styles.ProjectPanel}
+            >
+                <View style={{
+                    margin: 5,
+                }}>
+                    <Text style={styles.ProjectPanelText}>
+                        {item.desc}
+                    </Text>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: "space-between",
+                        margin: 5,
+                    }}>
+                        <Text style={{
+                            fontSize: 14,
+                            opacity: 0.8,
+                        }}>Duration:</Text>
+                        <Text style={{
+                            fontSize: 14,
+                            marginRight: 5,
+                            opacity: 0.8,
+                        }}>Organisation:</Text>
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: "space-between",
+                        margin: 5,
+                        marginTop: 0,
+                    }}>
+                        <Text style={{
+                            fontSize: 12,
+                        }}>{item.duration + ' months'}</Text>
+                        <Text style={{
+                            fontSize: 12,
+                        }}>{item.orgzn}</Text>
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 10,
+                        alignItems: 'center',
+                    }}>
+                        <FontAwesome5Icon
+                            name={'github'}
+                            style={{
+                                marginRight: 5,
+                            }}
+                            size={20}
+                        />
+                        <Text
+                            onPress={() => Linking.openURL(item.link)}>Github
+                            Link</Text>
+                    </View>
+                </View>
+            </Panel>
+        )
+    }
+
     render() {
         return (
             <View style={styles.ProjectBody}>
                 <View style={styles.ProjectList}>
                     <FlatList
                         showsVerticalScrollIndicator={false}
-                        data={[
-                            {
-                                id: 1,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 2,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 3,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 4,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 5,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 6,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative'
-                            },
-                            {
-                                id: 7,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                            },
-                            {
-                                id: 8,
-                                desc: 'Lorem ipsum dolor sit amet,\n' +
-                                    'consectetur adipiscistyng elit,\n' +
-                                    'sed do eiusmod tempor incididunt ut\n' +
-                                    'labore et dolore magna aliqua',
-                                link: 'https://github.com/Biditmangal/ReactNative/tree/master/MyReactNative',
-                                orgzn: 'UPES ACM student chapters',
-                                duration:2,
-                            },
-                        ]}
-                        renderItem={({item}) =>
-                            <Panel
-                                header={`Project ${item.id}`}
-                                style={styles.ProjectPanel}
-                            >
-                                <View style={{
-                                    margin: 5,
-                                }}>
-                                    <Text style={styles.ProjectPanelText}>
-                                        {item.desc}
-                                    </Text>
-                                    <View style={{
-                                        flexDirection: 'row',
-                                        justifyContent: "space-between",
-                                        margin: 5,
-                                    }}>
-                                        <Text style={{
-                                            fontSize: 14,
-                                            opacity: 0.8,
-                                        }}>Duration:</Text>
-                                        <Text style={{
-                                            fontSize: 14,
-                                            marginRight: 5,
-                                            opacity: 0.8,
-                                        }}>Organisation:</Text>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row',
-                                        justifyContent: "space-between",
-                                        margin: 5,
-                                        marginTop: 0,
-                                    }}>
-                                        <Text style={{
-                                            fontSize: 12,
-                                        }}>{item.duration + ' months'}</Text>
-                                        <Text style={{
-                                            fontSize: 12,
-                                        }}>{item.orgzn}</Text>
-                                    </View>
-                                    <View style={{
-                                        flexDirection: 'row',
-                                        marginLeft: 10,
-                                        alignItems: 'center',
-                                    }}>
-                                        <FontAwesome5Icon
-                                            name={'github'}
-                                            style={{
-                                                marginRight: 5,
-                                            }}
-                                            size={20}
-                                        />
-                                        <Text
-                                            onPress={() => Linking.openURL(item.link)}>Github
-                                            Link</Text>
-                                    </View>
-                                </View>
-                            </Panel>
-                        }
-
+                        data={this.dataToRender()}
+                        renderItem={({item}) =>this.renderListItem(item)}
                     />
                 </View>
             </View>
